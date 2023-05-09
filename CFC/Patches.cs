c4a83bfe686e7fe97d74296f20fe723c3920643f
sbtoonz <67915879+sbtoonz@users.noreply.github.com>
2023-05-09 12:28:16 -0700

    Flashy red text is annoying
    Signed-off-by: sbtoonz <67915879+sbtoonz@users.noreply.github.com>

--- CFC/Patches.cs
+++ CFC/Patches.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Reflection;
 using System.Reflection.Emit;
 using HarmonyLib;
@@ -8,36 +9,60 @@ namespace CFC
 {
     public static class Patches
     {
-        [HarmonyPatch(typeof(Player), nameof(Player.HaveRequirements), typeof(Piece), typeof(Player.RequirementMode))]
-        public static class RequirementsTranspiler
+      
+        /*
+         * IL_00e9: ldarg.2      // player
+    IL_00ea: callvirt     instance class Inventory Humanoid::GetInventory()
+    IL_00ef: ldarg.1      // req
+    IL_00f0: ldfld        class ItemDrop Piece/Requirement::m_resItem
+    IL_00f5: ldfld        class ItemDrop/ItemData ItemDrop::m_itemData
+    IL_00fa: ldfld        class ItemDrop/ItemData/SharedData ItemDrop/ItemData::m_shared
+    IL_00ff: ldfld        string ItemDrop/ItemData/SharedData::m_name
+    IL_0104: ldc.i4.m1
+    IL_0105: callvirt     instance int32 Inventory::CountItems(string, int32)
+    IL_010a: stloc.s      num
+         */
+        [HarmonyPatch(typeof(InventoryGui), nameof(InventoryGui.SetupRequirement))]
+        public static class SetupReqTranspiler
         {
-            private static readonly MethodInfo ComputeItemQuantity = AccessTools.Method(typeof(Requirements2Transpiler), nameof(Requirements2Transpiler.CheckChestList));
             [HarmonyTranspiler]
-            public static IEnumerable<CodeInstruction> HaveReqs(IEnumerable<CodeInstruction> instructions)
+            public static IEnumerable<CodeInstruction> SetupRequirementsTranspiler(IEnumerable<CodeInstruction> instructions)
             {
                 return new CodeMatcher(instructions)
                     .MatchForward(useEnd: false,
-                        new CodeMatch(OpCodes.Ldarg_0),
-                        new CodeMatch(OpCodes.Ldfld, AccessTools.Field(typeof(Humanoid), nameof(Humanoid.m_inventory))),
-                        new CodeMatch(OpCodes.Ldloc_2),
+                        new CodeMatch(OpCodes.Ldarg_2),
+                        new CodeMatch(OpCodes.Callvirt, AccessTools.Method(typeof(Humanoid), nameof(Humanoid.GetInventory))),
+                        new CodeMatch(OpCodes.Ldarg_1),
                         new CodeMatch(OpCodes.Ldfld, AccessTools.Field(typeof(Piece.Requirement), nameof(Piece.Requirement.m_resItem))),
-                        new CodeMatch(OpCodes.Ldfld, AccessTools.Field(typeof(ItemDrop), nameof(ItemDrop.m_itemData)))
-                    )
-                    .Advance(8)
-                    .InsertAndAdvance(new CodeInstruction(OpCodes.Ldloc_2))
-                    .InsertAndAdvance(new CodeInstruction(OpCodes.Ldarg_0))
-                    .InsertAndAdvance(new CodeInstruction(OpCodes.Call, ComputeItemQuantity))
+                        new CodeMatch(OpCodes.Ldfld, AccessTools.Field(typeof(ItemDrop), nameof(ItemDrop.m_itemData))))
+                    .Advance(9)
+                    .InsertAndAdvance(new CodeInstruction(OpCodes.Ldarg_1))
+                    .InsertAndAdvance(new CodeInstruction(OpCodes.Ldarg_2))
+                    .InsertAndAdvance(Transpilers.EmitDelegate<Func<int, Piece.Requirement, Player, int>>(SetupRequirementCheck))
                     .InstructionEnumeration();
             }
+
         }
         
+        
+        /*
+         * IL_0089: ldarg.0      // this
+      IL_008a: ldfld        class Inventory Humanoid::m_inventory
+      IL_008f: ldloc.2      // resource
+      IL_0090: ldfld        class ItemDrop Piece/Requirement::m_resItem
+      IL_0095: ldfld        class ItemDrop/ItemData ItemDrop::m_itemData
+      IL_009a: ldfld        class ItemDrop/ItemData/SharedData ItemDrop/ItemData::m_shared
+      IL_009f: ldfld        string ItemDrop/ItemData/SharedData::m_name
+      IL_00a4: ldc.i4.m1
+      IL_00a5: callvirt     instance int32 Inventory::CountItems(string, int32)
+      IL_00aa: stloc.s      num
+         */
+        
         [HarmonyPatch(typeof(Player), nameof(Player.HaveRequirementItems), typeof(Recipe), typeof(bool), typeof(int))]
-        public static class Requirements2Transpiler
+        public static class RequirementItemsTranspiler
         {
-            private static readonly MethodInfo ComputeItemQuantity = AccessTools.Method(typeof(Requirements2Transpiler), nameof(Requirements2Transpiler.CheckChestList));
-
             [HarmonyTranspiler]
-            public static IEnumerable<CodeInstruction> HaveReqs(IEnumerable<CodeInstruction> instructions)
+            public static IEnumerable<CodeInstruction> HaveReqsTranspiler(IEnumerable<CodeInstruction> instructions)
             {
                 return new CodeMatcher(instructions)
                     .MatchForward(useEnd: false,
@@ -50,38 +75,55 @@ namespace CFC
                     .Advance(9) 
                     .InsertAndAdvance(new CodeInstruction(OpCodes.Ldloc_2))
                     .InsertAndAdvance(new CodeInstruction(OpCodes.Ldarg_0))
-                    .InsertAndAdvance(new CodeInstruction(OpCodes.Call, ComputeItemQuantity))
+                    .InsertAndAdvance(Transpilers.EmitDelegate<Func<int,Piece.Requirement, Player, int>>(CheckChestList))
                     .InstructionEnumeration();
-            }
-
-            internal static int  CheckChestList(int fromInventory, Piece.Requirement item, Player player)
-            {
-                int i = 0;
-                ContainerPatch.Continers.RemoveAll(container => container == null);
-                foreach (var c in ContainerPatch.Continers)
-                {
-                    if(c == null) continue;
-                    if(Vector3.Distance(player.transform.position, c.transform.position) > CFCMod.ChestDistance?.Value)continue;
-                    if (c.m_inventory.HaveItem(item.m_resItem.m_itemData.m_shared.m_name))
-                    {
-                        i += c.m_inventory.CountItems(item.m_resItem.m_itemData.m_shared.m_name);
-                    }
-                }
-                
-                i += fromInventory;
-                return i;
             }
         }
 
-
-        [HarmonyPatch(typeof(Container), nameof(Container.Awake))]
-        public static class ContainerPatch
+        private static int  CheckChestList(int fromInventory, Piece.Requirement item, Player player)
         {
-            internal static readonly List<Container> Continers = new List<Container>();
+            int i = 0;
+            ContainerAwakePatch.Continers.RemoveWhere(container => container == null);
+            foreach (var c in ContainerAwakePatch.Continers)
+            {
+                if(c == null) continue;
+                if(Vector3.Distance(player.transform.position, c.transform.position) > CFCMod.ChestDistance?.Value)continue;
+                if (c.m_inventory.HaveItem(item.m_resItem.m_itemData.m_shared.m_name))
+                {
+                    i += c.m_inventory.CountItems(item.m_resItem.m_itemData.m_shared.m_name);
+                }
+            }
+            i += fromInventory;
+            return i;
+        }
+        
+        private static int  SetupRequirementCheck(int fromPlayerInv, Piece.Requirement item, Player player)
+        {
+            int i = 0;
+            ContainerAwakePatch.Continers.RemoveWhere(container => container == null);
+            foreach (var c in ContainerAwakePatch.Continers)
+            {
+                if(c == null) continue;
+                if(Vector3.Distance(player.transform.position, c.transform.position) > CFCMod.ChestDistance?.Value)continue;
+                if (c.m_inventory.HaveItem(item.m_resItem.m_itemData.m_shared.m_name))
+                {
+                    i += c.m_inventory.CountItems(item.m_resItem.m_itemData.m_shared.m_name);
+                }
+            }
+
+            i += fromPlayerInv;
+            return i;
+        }
+        
+        
+        [HarmonyPatch(typeof(Container), nameof(Container.Awake))]
+        public static class ContainerAwakePatch
+        {
+            internal static readonly HashSet<Container> Continers = new HashSet<Container>();
 
             public static void Postfix(Container __instance)
             {
-                if(!Continers.Contains(__instance))Continers.Add(__instance);
+                Continers.Add(__instance);
             }
         }
 
@@ -90,7 +132,7 @@ namespace CFC
         {
             public static void Prefix(Container __instance)
             {
-                if (ContainerPatch.Continers.Contains(__instance)) ContainerPatch.Continers.Remove(__instance);
+                if (ContainerAwakePatch.Continers.Contains(__instance)) ContainerAwakePatch.Continers.Remove(__instance);
             }
         }
     }
